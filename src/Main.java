c1e5420a1b61a7e0eeb58791a28c2f5fb0210966
srgesio <gesioiajm@gmail.com>
2018-02-27 22:37:22 -0300

    Criei interface com o usuário pela linha de comando baseada em esquema de menus.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,66 @@
 
+import java.util.Scanner;
+
+
 public class Main {
 
-}
+    public static void menuUI(){
+        System.out.println("PILHA DE ARRAY\n====================="
+                + "\nEscolha uma opção:\n----------------------\n"
+                + "1 - Adicionar elemento\n"
+                + "2 - Remover ultimo elemento\n"
+                + "3 - Ver ultimo elemento\n"
+                + "4 - Ver número de elementos na pilha\n"
+                + "5 - Ver se a pilha está vazia\n"
+                + "0 - SAIR");
+    }
+
+    public static void main(String args[]) throws PilhaVazia {
+        int tam=5;
+        Object elemento;
+        PilhaArray minhaPilha = new PilhaArray(tam);
+        int resposta=-1;
+        while (resposta!=0) {
+            menuUI();
+            Scanner zoy = new Scanner (System.in);
+            resposta=zoy.nextInt();
+            switch(resposta){
+                case 1:
+                    System.out.print("ADICIONAR: ");
+                    elemento = zoy.next();
+                    minhaPilha.push(elemento);
+                
+                case 2:
+                    minhaPilha.pop();
+                    System.out.println("Elemento Removido com sucesso!!");
+                    Object topo = minhaPilha.top();
+                    System.out.println("\n--->Seu elemento do topo agora é "+topo+"!<---");
+                
+                case 3:
+                    Object topo2 = minhaPilha.top();
+                    System.out.println("\n--->Seu elemento do topo é "+topo2+"!<---");
+                
+                case 4:
+                    int qtdElementos = minhaPilha.size();
+                    System.out.println("Você tem "+qtdElementos+" na pilha!");
+                
+                case 5:
+                    boolean situacao = minhaPilha.isEmpty();
+                    int qtd = minhaPilha.size();
+                    if (situacao==true){
+                        System.out.println("Sim! Pilha Vazia!");
+                    }else{
+                        System.out.println("Não! a pilha não está vazia. Contém "+ qtd +" elementos!");
+                    }
+                
+                case 0:
+                    System.out.println("FIM DA PILHA\n=======================================");
+                
+                default:
+                    System.out.println("Escolha uma opção válida!");
+            }
+            
+        }
+    
+    }
+}  
--- src/PilhaArray.java
+++ src/PilhaArray.java
@@ -4,18 +4,18 @@ public class PilhaArray implements Pilha {
 	private Object arrayPilha[];
 	private int t = -1;
 	
+        
+        //CONSTRUTOR DO OBJETO
 	public PilhaArray (int tam) {
 		arrayPilha = new Object[tam];
 	}
 	
-	public boolean isEmpty() {
-		return t == -1;
-	}
-	
-	public int size() {
-		return t+1;
-	}
-	
+        //ADCIONA ELEMENTO NO ARRAY
+        public void push(Object o){
+            
+        };
+        
+         //MOSTRA O ULTIMO ELEMENTO INSERIDO, SEM REMOVER
 	public Object top() throws PilhaVazia{
 		if (isEmpty())
 			throw new PilhaVazia();
@@ -24,10 +24,24 @@ public class PilhaArray implements Pilha {
 		
 	}
 	
+        //RETORNA SE ESTÁ VAZIO OU NÃO
+	public boolean isEmpty() {
+		return t == -1;
+	}
+	
+        //RETORNA O TAMANHO DO ARRAY
+	public int size() {
+		return t+1;
+	}
+	
+       
+        //REMOVE UM ELEMENTO E RETORNA O ULTIMO ELEMENTO INSERIDO
 	public Object pop() throws PilhaVazia{
 		if (isEmpty())
 			throw new PilhaVazia();
 		else
 			return arrayPilha[t--];
 	}
+        
+
 }
